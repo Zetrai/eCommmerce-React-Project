872239933fddded686181caf132d13601b45c674
zetra <karanpatadia28@gmail.com>
2022-10-08 17:23:30 +0530

    Add basic strucutre

--- royal-clothing/src/App.js
+++ royal-clothing/src/App.js
@@ -1,11 +1,54 @@
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <div>Royal Clothing</div>
+      <div className='categories-container'>
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>Hats</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
+      <div className='categories-container'>
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>Jackets</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
+      <div className='categories-container'>
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>Sneakers</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
+      <div className='categories-container'>
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>Womens</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
+      <div className='categories-container'>
+        <div className='category-container'>
+          {/* <img /> */}
+          <div className='category-body-container'>
+            <h2>Mens</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
