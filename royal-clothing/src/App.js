9a81e4d737a3fcb8003dfe196058fab12dc0a795
zetra <karanpatadia28@gmail.com>
2022-10-09 17:13:01 +0530

    Adding Basic Routing Structure

--- royal-clothing/src/App.js
+++ royal-clothing/src/App.js
@@ -1,38 +1,25 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
+import Home from './routes/home/home.component';
+
+const Navigation = () => {
   return (
-    <div className='App'>
-      <div>Royal Clothing</div>
-      <Directory categories={categories} />
+    <div>
+      <div>
+        <h1>Navigation Bar (0.0) </h1>
+      </div>
+      <Outlet />
     </div>
+  );
+};
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='home' element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
 
