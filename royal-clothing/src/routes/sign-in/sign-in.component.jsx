ae4752062fc927b45508d256b1d3fcca10ac16ba
zetra <karanpatadia28@gmail.com>
2022-10-09 20:37:39 +0530

    sign in using redirect method

--- royal-clothing/src/routes/sign-in/sign-in.component.jsx
+++ royal-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,36 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
+  signInWithGoogleRedirect,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    async function func() {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+      console.log(response);
+    }
+    func();
+  }, []);
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign in Page!</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
