5e92b2a75aebe218299197c4df89d412d4cf5de6
zetra <karanpatadia28@gmail.com>
2022-11-01 11:50:32 +0530

    Fixing the scss issue

--- royal-clothing/src/components/directory-item/directory-item.component.jsx
+++ royal-clothing/src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,20 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className='category-container'>
+    <div className='directory-item-container'>
       <div
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='category-body-container'>
+      <div className='body'>
         <h2>{title}</h2>
         <p>Show Now</p>
       </div>
     </div>
   );
 };
-export default CategoryItem;
+export default DirectoryItem;
