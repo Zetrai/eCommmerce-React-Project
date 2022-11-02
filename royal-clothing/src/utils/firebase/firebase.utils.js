273209be49e421ba8b4d89c72ef65b08bf5d0420
zetra <karanpatadia28@gmail.com>
2022-11-02 17:28:56 +0530

    Removing Warnings and end of project

--- royal-clothing/src/utils/firebase/firebase.utils.js
+++ royal-clothing/src/utils/firebase/firebase.utils.js
@@ -31,7 +31,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
